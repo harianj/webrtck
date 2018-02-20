6ead2fa6b1d2dba31146197c5cf33ae492fba251
unknown <U16549@D510LTRV.ustr.com>
2018-02-20 18:16:29 +0530

    initial

--- get_cifar10.sh
+++
@@ -1,8 +0,0 @@
-DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-cd "$DIR"
-echo "Downloading..."
-wget --no-check-certificate http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz
-echo "Unzipping..."
-tar -xf cifar-10-binary.tar.gz && rm -f cifar-10-binary.tar.gz
-mv cifar-10-batches-bin/* . && rm -rf cifar-10-batches-bin
-echo "Done."
